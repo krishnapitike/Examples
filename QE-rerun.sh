ed745b7fe6644a0515325dfcf50f136ca989685f
krishnapitike <50116547+krishnapitike@users.noreply.github.com>
2019-05-01 17:22:05 -0400

    Update QE-rerun.sh
    Made many changes to update the filenames and the script is more general

--- QE-rerun.sh
+++ QE-rerun.sh
@@ -17,8 +17,15 @@ module load QE/6.1
 cd  $PBS_O_WORKDIR
 pwd
 
-max=
-system=
+natm=      #number of atoms
+max=       #maximum number of iterations
+system=    #name of the system
+phase=     #cubic, tetragonal etc
+strain=    #bulk, st_-1, etc
+calc=      #pw.vc-relax, pw.scf etc
+fname=     #dont worry if not specified
+fname=`echo ${system}.${natm}atom.${phase}.${strain}.${calc}`
+
 mkdir input
 cp    *     input/
 
@@ -27,24 +34,24 @@ for i in `seq 1 9` ; do if [ -a lattice.$i ] ; then m=`echo $((i+1))` ; fi ; don
 for n in `seq $m $max` ; do
 
   if [ $m -gt 1 ] ; then
-    grep -A69 "CELL_PARAMETERS (angstrom)" ${system}.64atom.pseudocubic.bulk.pw.vc-relax.`echo $((n-1))`.out > temp.allcelldata
-    tail -70  temp.allcelldata                                                                               > temp.celldata
-    head -4   temp.celldata                                                                                  > lattice.${n}
-    tail -65  temp.celldata                                                                                  > coord.${n}
-    rm        temp.*
+    grep -A`echo $((natm+5))` "CELL_PARAMETERS (angstrom)" ${fname}.`echo $((n-1))`.out > temp.allcelldata
+    tail -`echo $((natm+6))`  temp.allcelldata                                                                                  > temp.celldata
+    head -4                   temp.celldata                                                                                                     > lattice.${n}
+    tail -`echo $((natm+1))`  temp.celldata                                                                                     > coord.${n}
+    rm                        temp.*
   fi
 
   if [ -s lattice.$n ] ; then echo "lattice.${n} is good" ; else cp lattice.`echo $((n-1))` lattice.${n} ; fi
   if [ -s coord.$n ]   ; then echo "coord.${n}   is good" ; else cp coord.`echo $((n-1))`   coord.${n}   ; fi
   
-  cat       incar potcar lattice.${n} kpoints coord.${n}                                        > ${system}.64atom.pseudocubic.bulk.pw.vc-relax.${n}.inp
-  mpirun -n 72 pw.x -nk 2 -nb 6 -input ${system}.64atom.pseudocubic.bulk.pw.vc-relax.${n}.inp   > ${system}.64atom.pseudocubic.bulk.pw.vc-relax.${n}.out
+  cat       incar potcar lattice.${n} kpoints coord.${n} > ${fname}.${n}.inp
+  mpirun -n 72 pw.x -nk 2 -nb 6 -input ${fname}.${n}.inp > ${fname}.${n}.out
 
-  grep -A69 "CELL_PARAMETERS (angstrom)" ${system}.64atom.pseudocubic.bulk.pw.vc-relax.${n}.out > temp.allcelldata
-  tail -70  temp.allcelldata                                                                    > temp.celldata
-  head -4   temp.celldata                                                                       > lattice.`echo $((n+1))`
-  tail -65  temp.celldata                                                                       > coord.`echo $((n+1))`
-  rm        temp.*
+  grep -A`echo $((natm+5))` "CELL_PARAMETERS (angstrom)" ${fname}.${n}.out > temp.allcelldata
+  tail -`echo $((natm+6))`  temp.allcelldata                               > temp.celldata
+  head -4                   temp.celldata                                  > lattice.`echo $((n+1))`
+  tail -`echo $((natm+1))`  temp.celldata                                  > coord.`echo $((n+1))`
+  rm                        temp.*
 
 done
 rm -r scratch
