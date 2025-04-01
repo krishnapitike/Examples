96203d2aceda55f7be1fa10fdd69e5c065771ca6
krishnapitike <50116547+krishnapitike@users.noreply.github.com>
2025-03-31 19:11:07 -0700

    Update ase_commands.py

--- ase_commands.py
+++ ase_commands.py
@@ -1,11 +1,16 @@
 from ase.io.lammpsdata import read_lammps_data, write_lammps_data
+from ase.build import make_supercell
 
 #################################################################
 # reading and writing lammps data
 #################################################################
 # reading lammps data
-sys=read_lammps_data('init.data',style='atomic')
+unit_cell=read_lammps_data('init.data',style='atomic')
+
+# making supercell
+transformation = [[2,0,0],[0,2,0],[0,0,2]]
+super_cell=make_supercell(unit_cell, transformation)
 
 # writing lammps data
-write_lammps_data('test.data',sys,atom_style='atomic')
+write_lammps_data('supercell.data',super_cell,atom_style='atomic')
 
